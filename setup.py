a9c01ee2f1ca2035286081d4ea8cf1fb0e73cc43
Chanyeong <104907351+ChanyeongKim@users.noreply.github.com>
2022-08-18 10:08:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ setup(
     version="1.0",
     author = "Chan Yeong Kim",
     author_email='vxterran@yonsei.ac.kr',
-    #description = "Video datasets' annotation parser and etc.",
-    #url = "https://github.com/kiyoon/video_datasets_api",
-    #packages=find_packages(),
     packages=['cmagfilter'],
-    #package_dir={'video_datasets_api': 'src'},
     python_requires='>=3.6',
 )
